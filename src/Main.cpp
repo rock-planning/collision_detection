8b1578750f9a3483ac30e9dcbbbd2aeeaee3e6d1
Sankar Natarajan <sankar.natarajan@dfki.de>
2018-03-02 12:18:58 +0100

    Initial commit

---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <collision_detection/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    collision_detection::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
